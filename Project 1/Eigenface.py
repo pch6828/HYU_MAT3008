a739b66a8412f7821cccdedf27f04a3c974b82cb
pch6828 <pch6828@hanyang.ac.kr>
2020-10-11 19:52:45 +0900

    project 1

--- Project 1/Eigenface.py
+++ Project 1/Eigenface.py
@@ -30,36 +30,120 @@ def showVectorAsImage(vector, image_name):
     image.putdata(vector)
     image.save(image_name)
 
+def saveResultImage(byte_array, image_name):
+    image = Image.new('L', (32, 32))
+    image.putdata(byte_array)
+    image.save(image_name)
+
+def getCoefficient(name, testset, eigenfaces, mean_face):
+    result = np.zeros((eigenfaces.shape[1],))
+    for i in range(5):
+        filename = testset+str(i)+'.jpg'
+        image = np.asarray(
+            Image.open(filename, 'r').convert('L')
+            ).reshape((1024,))
+
+        x = []
+        for row in eigenfaces.T:
+           x.append((image@row))
+        saveResultImage(eigenfaces@np.array(x)+mean_face,
+                        testset+'result'+str(i)+'.jpg')
+        result += np.array(x) 
+    result /= 5
+    return (name, result)
+
+def processTest(coefficient, eigenfaces, filename):
+    image = np.asarray(
+        Image.open(filename, 'r').convert('L')
+        ).reshape((1024,))
+    x = []
+    for row in eigenfaces.T:
+        x.append((image@row))
+    x = np.array(x)
+    
+    dist = None
+    result = None
+    for name, ce in coefficient:
+        v = x - ce
+        if dist != None:
+            if dist > np.dot(v, v):
+                dist = np.dot(v, v)
+                result = name
+        else:
+            dist = np.dot(v, v)
+            result = name
+    return result
+
 def main():
-    print('Convert Images to Matrix...')
+    print('@Convert Images to Matrix...')
     arr = np.empty((1, 1024), dtype=int)
     for filename in os.listdir('rawdata/'):
         row = np.array([resizeByte(getByteArray('rawdata/'+filename))])
         arr = np.append(arr, row, axis = 0)
     arr = np.delete(arr, [0,0], axis = 0)
-    print('Convert Complete')
-    print('Calculate Singular Value Decomposition...')
+    print('@Convert Complete')
+    print('')
+    print('@Calculate Singular Value Decomposition...')
     mean_vector = np.zeros((arr.shape[1],), dtype=int)
     for row in arr:
         mean_vector += row
     mean_vector //= arr.shape[0]
-
+    
     for i in range(arr.shape[0]):
         arr[i] -= mean_vector
     arr = arr.T
 
     U, C, V = np.linalg.svd(arr, full_matrices = False)
     U = U.T
-    print('SVD Complete')
-    print('Visualize Vectors')
+    visualized_U = np.empty((1024, 1024), dtype=int)
+    print('@SVD Complete')
+    print('')
+    print('@Visualize Vectors')
     for i in range(U.shape[0]):
         minimum = min(U[i])
         maximum = max(U[i])
         for j in range(U.shape[1]):
-            U[i][j] = int(255*(U[i][j] - minimum)/(maximum-minimum))
-        showVectorAsImage(U[i], 'eigenfaces/'+str(i)+'.jpg')
-        
-    print('Visualize Complete')
+            visualized_U[i][j] = int(255*(U[i][j] - minimum)/(maximum-minimum))
+        showVectorAsImage(visualized_U[i], 'eigenfaces/'+str(i)+'.jpg')
+    
+    print('@Visualize Complete')
+    print('')
+    print('@Construct Face Recognition Information...')
+    eigenfaces = U[:70].T
+    TC = [('Bill_Clinton','test_sample/test0/'),
+          ('George_W_Bush','test_sample/test1/'),
+          ('Sylvester_Stallone','test_sample/test2/'),
+          ('Yoriko_Kawaguchi','test_sample/test3/'),
+          ('Vladimir_Putin','test_sample/test4/'),
+          ('Tom_Hanks','test_sample/test5/'),
+          ('Roh_Moo_hyun','test_sample/test6/'),
+          ('Michael_Jackson','test_sample/test7/'),
+          ('Leonardo_Dicaprio','test_sample/test8/'),
+          ('Britney_Spears','test_sample/test9/')]
+    idx = 0
+    coefficient = []
+    for name, testset in TC:
+        coefficient.append(getCoefficient(name, testset, eigenfaces, mean_vector))
+        idx+=1
+    print('@Construct Complete')
+    print('')
+    print('@Face Recognition Test...')
+    cmd = input('Select Mode > [1] - Manual | [2] - All file : ')
+    if cmd == '1':
+        while True:
+            filename = input('Input Filename, or "quit" to quit: ')
+            if filename == 'quit':
+                break
+            print(processTest(coefficient, eigenfaces, filename))
+
+    elif cmd == '2':
+        for i in range(10):
+            print('Test #'+str(i)+' '+TC[i][0])
+            for j in range(5):
+                filename = TC[i][1]+str(j)+'.jpg'
+                print(processTest(coefficient, eigenfaces, filename))
+            print('')
+    print('@Test Complete')
     
 if __name__ == "__main__":
     main()
--- Project 1/ParseSample.py
+++ Project 1/ParseSample.py
@@ -11,13 +11,32 @@ def resizeImage(image_name, save_name):
 def parseTestSet(src, dist):
     i = 0
     for filename in os.listdir('test_sample/'+src):
-        resizeImage('test_sample/'+src+filename, 'test_sample/'+dist+str(i)+'.jpg')
+        resizeImage('test_sample/'+src+filename,
+                    'test_sample/'+dist+str(i)+'.jpg')
         i+=1
 
 def main():
-    srcs = ['Bill_Clinton/', 'George_W_Bush/', 'Sylvester_Stallone/']
-    dists = ['test0/', 'test1/', 'test2/']
-    for i in range(3):
+    srcs = ['Bill_Clinton/',
+            'George_W_Bush/',
+            'Sylvester_Stallone/',
+            'Yoriko_Kawaguchi/',
+            'Vladimir_Putin/',
+            'Tom_Hanks/',
+            'Roh_Moo_hyun/',
+            'Michael_Jackson/',
+            'Leonardo_Dicaprio/',
+            'Britney_Spears/']
+    dists = ['test0/',
+             'test1/',
+             'test2/',
+             'test3/',
+             'test4/',
+             'test5/',
+             'test6/',
+             'test7/',
+             'test8/',
+             'test9/']
+    for i in range(10):
         parseTestSet(srcs[i], dists[i])
 
 if __name__ == "__main__":
