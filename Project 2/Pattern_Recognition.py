b8ea3369880218ce83c0b5c60281ba0d55baae45
pch6828 <pch6828@hanyang.ac.kr>
2020-12-11 20:14:19 +0900

    Project 2 : finish

--- Project 2/Pattern_Recognition.py
+++ Project 2/Pattern_Recognition.py
@@ -5,6 +5,12 @@ import scipy.stats as st
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
+# multiplier for mean constant
+MEAN_CONSTANT = 1
+# constant for variance
+VAR_CONSTANT = 1
+
+
 def dist(a, b):
     return np.sqrt(np.dot(a - b, a - b))
 
@@ -15,7 +21,7 @@ def make_cluster(mx, my, mz, vx, vy, vz, cnt):
     result = []
     for i in range(cnt):
         result.append((x[i], y[i], z[i]))
-    return result
+    return result, x, y, z
 
 def get_criterion(data):
     km = KMeans(n_clusters=5, random_state=0)
@@ -65,38 +71,48 @@ def recognition(criterion, maximum_dist, data, expected):
     return cnt/total*100
 
 def main(): 
+    global MEAN_CONSTANT, VAR_CONSTANT
     learn_data = []
-    cluster_seed = [(0,0,0,1,1,1),
-                    (5,6,7,1,1,1),
-                    (1,2,3,1,1,1),
-                    (10,7,1,1,1,1),
-                    (1,9,7,1,1,1)]
+    cluster_seed = [(0*MEAN_CONSTANT,0*MEAN_CONSTANT,0*MEAN_CONSTANT,1+VAR_CONSTANT,1+VAR_CONSTANT,1+VAR_CONSTANT),
+                    (5*MEAN_CONSTANT,6*MEAN_CONSTANT,7*MEAN_CONSTANT,2+VAR_CONSTANT,0.8+VAR_CONSTANT,1.5+VAR_CONSTANT),
+                    (1*MEAN_CONSTANT,2*MEAN_CONSTANT,3*MEAN_CONSTANT,0.8+VAR_CONSTANT,0.5+VAR_CONSTANT,0.8+VAR_CONSTANT),
+                    (10*MEAN_CONSTANT,7*MEAN_CONSTANT,1*MEAN_CONSTANT,2+VAR_CONSTANT,3+VAR_CONSTANT,2+VAR_CONSTANT),
+                    (3*MEAN_CONSTANT,12*MEAN_CONSTANT,7*MEAN_CONSTANT,1.5+VAR_CONSTANT,1+VAR_CONSTANT,0.5+VAR_CONSTANT)]
     means = []
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
     print("Making Random Clustered Data... (for Learning)")
     for mx, my, mz, vx, vy, vz in cluster_seed:
-        learn_data+=make_cluster(mx, my, mz, vx, vy, vz, 300)
+        cluster,x,y,z = make_cluster(mx, my, mz, vx, vy, vz, 300)
+        learn_data+=cluster
+        ax.scatter(x,y,z,marker='o')
         means.append((mx,my,mz))
     
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    #plt.show()
+    ax.view_init(35, -35)
+    plt.savefig('plot.jpg')
     print("Evaluate Clustering...")
     criterion, maximum_dist = get_criterion(learn_data)
 
     criterion = sort_criterion(criterion, means)
     print("Making Random Clustered Data... (for Testing)")
-    cluster_seed.append((3,4,3,1,1,1))
+    cluster_seed.append((-1*MEAN_CONSTANT,4*MEAN_CONSTANT,-3*MEAN_CONSTANT,0.5+VAR_CONSTANT,0.7+VAR_CONSTANT,0.5+VAR_CONSTANT))
 
     test_data = []
     expected = []
     for i in range(6):
         mx, my, mz, vx, vy, vz = cluster_seed[i]
-        test_data+=make_cluster(mx, my, mz, vx, vy, vz, 100)
-        for j in range(100):
+        cluster,x,y,z = make_cluster(mx, my, mz, vx, vy, vz, 100)
+        test_data+=cluster
+        for _ in range(100):
             expected.append(i)
-    
     
     print("Testing...")
     print("Clustering Accuracy : %f%%" % (recognition(criterion,maximum_dist,test_data,expected)))
     
-
-
 if __name__ == "__main__":
     main()
